3325194f94289fa5812df67598415a00b0cdd3ab
zmdnz <35179441+zmdnz@users.noreply.github.com>
2018-05-10 01:20:10 +0100

    Added Service Charge calculation
    Added Service Charge calculation

--- scala/com/cafex/customer/Billing/CustomerBillCalc.scala
+++ scala/com/cafex/customer/Billing/CustomerBillCalc.scala
@@ -8,14 +8,29 @@ object CustomerBillCalc {
                         "Cheese Sandwich"->2.50,
                         "Steak Sandwich"->4.50)
                          
+ /* Grouped based on service charge category */                      
+  val drinks = List("Cola","Cofee")
+  val hotFood = List("Steak Sandwich")
+  val coldFood = List("Cheese Sandwich")  
+  
   /** Calculates service charge and total bill
   * @param list of items customer purchased 
   * returns 2 digit decimal precision total bill
   * 
   */  
   def billCalculator(bill: List[String])={ 
-    
-    val total_bill = BigDecimal((bill map cafex_menu).sum).setScale(2) 
+    var service_charge = 0.00   
+    val total_without_service_charge = (bill map cafex_menu).sum   
+    if (bill.exists{hotFood.contains}) 
+    {
+      service_charge = (total_without_service_charge/100)*20 
+      if (service_charge > 20.00) service_charge = 20.00       
+    }
+    else if (bill.exists{coldFood.contains})  
+    {
+      service_charge = (total_without_service_charge/100)*10 
+    }  
+    val total_bill = BigDecimal(total_without_service_charge + service_charge).setScale(2)    
     total_bill     
   } 
 }
--- scala/com/cafex/customer/Billing/CustomerBillCalcTest.scala
+++ scala/com/cafex/customer/Billing/CustomerBillCalcTest.scala
@@ -7,16 +7,48 @@ import org.junit.Assert._
 class CustomerBillCalcTest{    
     
   @Test
-  def testTotalBillWithAllItems()
+  def testDrinksOnlyBillHasNoServiceCharge()
   {
-    assertEquals(CustomerBillCalc.billCalculator(List("Coffee","Cola", "Cheese Sandwich", "Steak Sandwich")), 8.50) 
+    assertEquals(CustomerBillCalc.billCalculator(List("Coffee","Cola", "Cola")), 2.00)    
+  }
+  
+  @Test
+  def testColdFoodOnlyBillCharged10PercentServiceCharge()
+  {
+    assertEquals(CustomerBillCalc.billCalculator(List("Cheese Sandwich")), 2.75)  
+  }
+  
+  @Test
+  def testHotFoodOnlyBillCharged20PercentServiceCharge()
+  {
+    assertEquals(CustomerBillCalc.billCalculator(List("Steak Sandwich")), 5.40) 
+  }   
+   
+  @Test
+  def testHotFoodInclusionAdds20PercentServiceCharge()
+  {
+    assertEquals(CustomerBillCalc.billCalculator(List("Steak Sandwich","Coffee","Cola","Cola")), 7.80) 
+  }
+  
+  @Test
+  def testColdFoodAndDrinksCharged10Percent()
+  {
+    assertEquals(CustomerBillCalc.billCalculator(List("Cheese Sandwich", "Coffee")), 3.85) 
     
   }
   
   @Test
-  def testTotalBillWithFewItems()
+  def testColdFoodAndHotFoodWithoutDrinkCharged20Percent()
   {
-    assertEquals(CustomerBillCalc.billCalculator(List("Coffee", "Cheese Sandwich")), 3.50) 
-    
-  } 
+    assertEquals(CustomerBillCalc.billCalculator(List("Cheese Sandwich", "Steak Sandwich")), 8.40)
+  }
+  
+  @Test
+  def testServiceCharegeAbobe20GBP()
+  {
+    assertEquals(CustomerBillCalc.billCalculator(List("Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+                                                       "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+                                                       "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich","Cheese Sandwich","Cheese Sandwich","Cheese Sandwich","Cheese Sandwich",
+                                                       "Cheese Sandwich","Cheese Sandwich","Cheese Sandwich","Cheese Sandwich","Cheese Sandwich","Cheese Sandwich")), 135.00)
+  }
 }
